994a2fa275c1dbb3ed24cd5694aac9ec5c2b53d1
Adrian DC <radian.dc@gmail.com>
2021-01-03 18:26:33 +0100

    configs: Remove the unused and deprecated thermald.conf file
     * Blue devices use thermanager since 2016, I must have kept
       the file for reference and forgot to remove it later on
    
    Change-Id: Icd3451f8115e2b5022751f7ddeb2d8b802298584

--- product/thermal.mk
+++ product/thermal.mk
@@ -1,6 +1,5 @@
 # Thermal configuration
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/thermald.conf:system/etc/thermald.conf \
     $(COMMON_PATH)/configs/thermanager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/thermanager.xml
 
 # Thermal management packages
